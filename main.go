73f59f9d40e3e04b4201c7794e0930f1a6421b3f
root <sifulinyt@gmail.com>
2024-06-30 17:32:52 +0800

    update verify

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 
 	execute "sifu-box/Execute"
@@ -89,6 +90,33 @@ func main() {
         server.Use(middleware.Logger(), middleware.Recovery(true), cors.New(middleware.Cors()))
         // 设置API路由
         api_group := server.Group("/api")
+        api_group.GET("verify",func(ctx *gin.Context) {
+            // 尝试获取Authorization头字段
+            header := ctx.GetHeader("Authorization")
+            // 如果头字段为空,表示令牌不存在,返回未授权响应并终止当前请求
+            if header == "" {
+                ctx.JSON(http.StatusUnauthorized, gin.H{"error": "unauthorized"})
+                return
+            }
+
+            // 尝试获取服务器配置,特别是其中的键值,用于令牌验证
+            server_config, err := utils.Get_value("Server")
+            // 如果获取配置失败,记录错误并终止当前请求
+            if err != nil {
+                utils.Logger_caller("Get key failed!", err, 1)
+                return
+            }
+            // 从配置中提取键值
+            key := server_config.(utils.Server_config).Key
+
+            // 比较请求头中的令牌和配置中的键值
+            if key == header {
+                // 如果令牌有效,设置token到上下文中,供后续使用
+                ctx.JSON(http.StatusOK, gin.H{"message":true})
+                return
+            }
+            ctx.JSON(http.StatusUnauthorized, gin.H{"error": "unauthorized"})
+        })
         router.Setting_server(api_group)
         router.Setting_box(api_group)
         router.Setting_exec(api_group, &lock,cron_task,&cron_id)
