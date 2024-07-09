d5d353ff7a021bb619aba031e2f6b0022d3768d6
root <sifulinyt@gmail.com>
2024-07-09 20:55:15 +0800

    edit the config json format

--- Utils/struct.go
+++ Utils/struct.go
@@ -1,27 +1,27 @@
 package utils
 
 type Box_url struct {
-	Path   string `yaml:"path"`
-	Proxy  bool   `yaml:"proxy"`
-	Label  string `yaml:"label"`
-	Remote bool   `yaml:"remote"`
+	Path   string `yaml:"path" json:"path"`
+	Proxy  bool   `yaml:"proxy" json:"proxy"`
+	Label  string `yaml:"label" json:"label"`
+	Remote bool   `yaml:"remote" json:"remote"`
 }
 type Ruleset_value struct {
-	Path            string `yaml:"path"`
-	Format          string `yaml:"format"`
-	Type            string `yaml:"type"`
-	China           bool   `yaml:"china"`
-	Update_interval string `yaml:"update_interval"`
-	Download_detour string `yaml:"download_detour"`
+	Path            string `yaml:"path" json:"path"`
+	Format          string `yaml:"format" json:"format"`
+	Type            string `yaml:"type" json:"type"`
+	China           bool   `yaml:"china" json:"china"`
+	Update_interval string `yaml:"update_interval" json:"update_interval"`
+	Download_detour string `yaml:"download_detour" json:"download_detour"`
 }
 type Box_ruleset struct {
-	Label string        `yaml:"label"`
-	Value Ruleset_value `yaml:"value"`
+	Label string        `yaml:"label" json:"label"`
+	Value Ruleset_value `yaml:"value" json:"value"`
 }
 
 type Box_config struct {
-	Url      []Box_url     `yaml:"url"`
-	Rule_set []Box_ruleset `yaml:"rule_set"`
+	Url      []Box_url     `yaml:"url" json:"url"`
+	Rule_set []Box_ruleset `yaml:"rule_set" json:"rule_set"`
 }
 type Cors struct {
 	Origins []string `yaml:"origins"`
