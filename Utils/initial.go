451fccd856bc5d2ec094ec343b1b89cc3bc9e39b
linsifu <1982209396@qq.com>
2024-08-08 01:07:33 +0800

    update

--- Utils/initial.go
+++ Utils/initial.go
@@ -129,8 +129,8 @@ func LoadTemplate() error{
     return nil
 }
 func GetProjectDir() string {
-	// base_dir := filepath.Dir(os.Args[0])
-	// base_dir := "E:/Myproject/sifu-clash"
-	base_dir := "/root/sifu-clash"
+	base_dir := filepath.Dir(os.Args[0])
+	// base_dir := "E:/Myproject/sifu-box"
+	// base_dir := "/root/sifu-clash"
 	return base_dir
 }
