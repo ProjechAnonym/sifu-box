c5b024edee6c895fc398ce99f6f668647b70e6b4
ProjechAnonym <sifulinyt@gmail.com>
2025-01-14 11:30:52 +0800

    update

--- sifu-box-pages/src/utils/template/FetchTemplate.ts
+++ sifu-box-pages/src/utils/template/FetchTemplate.ts
@@ -2,12 +2,9 @@ import axios from "axios";
 
 export async function FetchTemplate(secret: string) {
   try {
-    const res = await axios.get(
-      "http://192.168.213.128:8080/api/templates/fetch",
-      {
-        headers: { Authorization: secret },
-      }
-    );
+    const res = await axios.get("/api/templates/fetch", {
+      headers: { Authorization: secret },
+    });
     return res.status === 200 ? res.data.message : null;
   } catch (e) {
     console.error(e);
