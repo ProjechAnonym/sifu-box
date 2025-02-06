16fa4720c8bab421d64b698c3178d673e8a99f12
root <sifulinyt@gmail.com>
2025-02-06 17:13:47 +0800

    updtae

--- middleware/cors.go
+++ middleware/cors.go
@@ -9,9 +9,9 @@ import (
 func Cors() cors.Config {
 	config := cors.Config{
 		AllowOrigins:     []string{"*"},
-		AllowMethods:     []string{"PUT", "PATCH", "POST", "GET","DELETE"},
+		AllowMethods:     []string{"PUT", "PATCH", "POST", "GET", "DELETE"},
 		AllowHeaders:     []string{"Origin", "domain", "scheme", "Authorization", "content-type"},
-		ExposeHeaders:    []string{"Content-Length", "Content-Type"},
+		ExposeHeaders:    []string{"Content-Length", "Content-Type", "Content-Disposition"},
 		AllowCredentials: true,
 		MaxAge:           12 * time.Hour,
 	}
