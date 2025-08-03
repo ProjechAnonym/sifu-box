89b283f3727eb5bfd913d645dbe0b2898dd489fb
ProjechAnonym <root@debian.debian>
2025-08-03 22:43:16 +0800

    update

--- models/ntp.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type NTP struct {
-	Enabled    bool   `json:"enabled,omitempty" yaml:"enabled,omitempty"`
-	Server     string `json:"server,omitempty" yaml:"server,omitempty"`
-	ServerPort int    `json:"server_port,omitempty" yaml:"server_port,omitempty"`
-	Interval   string `json:"interval,omitempty" yaml:"interval,omitempty"`
-	Dial       `json:",inline" yaml:",inline"`
-}
