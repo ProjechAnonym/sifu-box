4ffcea8268fbc993ea0a1769ffb15efadc93bfe9
ProjechAnonym <root@debian.debian>
2025-08-09 23:03:11 +0800

    udpate

--- template/ntp.go
+++
@@ -1,8 +0,0 @@
-package template
-
-type Ntp struct {
-	Enabled     bool   `json:"enabled" yaml:"enabled"`
-	Server      string `json:"server" yaml:"server"`
-	Server_port int    `json:"server_port,omitempty" yaml:"server_port,omitempty"`
-	Interval    string `json:"interval,omitempty" yaml:"interval,omitempty"`
-}
