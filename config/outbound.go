8f59e91089c6205e4a8a999b567d5fb9fb06be27
ProjechAnonym <root@debian.debian>
2025-08-10 10:54:54 +0800

    updatye

--- config/outbound.go
+++
@@ -1,7 +0,0 @@
-package config1
-
-type OutboundGroup struct {
-	Type      string   `json:"type" yaml:"type"`
-	Tag       string   `json:"tag" yaml:"tag"`
-	Providers []string `json:"providers" yaml:"providers"`
-}
