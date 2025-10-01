ade7e4bae0eed6db06fe850c747b7877fe4d5a09
ProjechAnonym <sifulinyt@gmail.com>
2025-10-01 17:49:14 +0800

    update

--- control/migrate.go
+++ control/migrate.go
@@ -6,34 +6,35 @@ import (
 	"sifu-box/ent"
 	"sifu-box/model"
 
+	"github.com/gin-gonic/gin"
 	"github.com/tidwall/buntdb"
 	"go.uber.org/zap"
 	"gopkg.in/yaml.v3"
 )
 
-func Migrate(ent_client *ent.Client, bunt_client *buntdb.DB, logger *zap.Logger) ([]byte, error) {
+func Export(ent_client *ent.Client, bunt_client *buntdb.DB, logger *zap.Logger) ([]byte, error) {
 	providers, err := ent_client.Provider.Query().All(context.Background())
 	if err != nil {
 		logger.Error(fmt.Sprintf(`查询机场数据失败: [%s]`, err.Error()))
 		return nil, fmt.Errorf(`查询机场数据失败: [%s]`, err.Error())
 	}
 	provider_list := []struct {
-		model.Provider
-		UUID      string           `json:"uuid,omitempty" yaml:"uuid,omitempty"`
-		Nodes     []map[string]any `json:"nodes,omitempty" yaml:"nodes,omitempty"`
-		Templates []string         `json:"templates,omitempty" yaml:"templates,omitempty"`
+		model.Provider `json:",inline" yaml:",inline"`
+		UUID           string           `json:"uuid,omitempty" yaml:"uuid,omitempty"`
+		Nodes          []map[string]any `json:"nodes,omitempty" yaml:"nodes,omitempty"`
+		Templates      []string         `json:"templates,omitempty" yaml:"templates,omitempty"`
 	}{}
 	ruleset_list := []struct {
-		model.Ruleset
-		Templates []string `json:"templates,omitempty" yaml:"templates,omitempty"`
+		model.Ruleset `json:",inline" yaml:",inline"`
+		Templates     []string `json:"templates,omitempty" yaml:"templates,omitempty"`
 	}{}
 	template_list := []model.Template{}
 	for _, provider := range providers {
 		provider_list = append(provider_list, struct {
-			model.Provider
-			UUID      string           `json:"uuid,omitempty" yaml:"uuid,omitempty"`
-			Nodes     []map[string]any `json:"nodes,omitempty" yaml:"nodes,omitempty"`
-			Templates []string         `json:"templates,omitempty" yaml:"templates,omitempty"`
+			model.Provider `json:",inline" yaml:",inline"`
+			UUID           string           `json:"uuid,omitempty" yaml:"uuid,omitempty"`
+			Nodes          []map[string]any `json:"nodes,omitempty" yaml:"nodes,omitempty"`
+			Templates      []string         `json:"templates,omitempty" yaml:"templates,omitempty"`
 		}{
 			Provider: model.Provider{
 				Name:   provider.Name,
@@ -51,8 +52,8 @@ func Migrate(ent_client *ent.Client, bunt_client *buntdb.DB, logger *zap.Logger)
 	}
 	for _, ruleset := range rulesets {
 		ruleset_list = append(ruleset_list, struct {
-			model.Ruleset
-			Templates []string `json:"templates,omitempty" yaml:"templates,omitempty"`
+			model.Ruleset `json:",inline" yaml:",inline"`
+			Templates     []string `json:"templates,omitempty" yaml:"templates,omitempty"`
 		}{
 			Templates: ruleset.Templates,
 			Ruleset: model.Ruleset{
@@ -85,14 +86,14 @@ func Migrate(ent_client *ent.Client, bunt_client *buntdb.DB, logger *zap.Logger)
 	}
 	content, err := yaml.Marshal(struct {
 		Providers []struct {
-			model.Provider
-			UUID      string           `json:"uuid,omitempty" yaml:"uuid,omitempty"`
-			Nodes     []map[string]any `json:"nodes,omitempty" yaml:"nodes,omitempty"`
-			Templates []string         `json:"templates,omitempty" yaml:"templates,omitempty"`
+			model.Provider `json:",inline" yaml:",inline"`
+			UUID           string           `json:"uuid,omitempty" yaml:"uuid,omitempty"`
+			Nodes          []map[string]any `json:"nodes,omitempty" yaml:"nodes,omitempty"`
+			Templates      []string         `json:"templates,omitempty" yaml:"templates,omitempty"`
 		} `json:"providers,omitempty" yaml:"providers,omitempty"`
 		Rulesets []struct {
-			model.Ruleset
-			Templates []string `json:"templates,omitempty" yaml:"templates,omitempty"`
+			model.Ruleset `json:",inline" yaml:",inline"`
+			Templates     []string `json:"templates,omitempty" yaml:"templates,omitempty"`
 		} `json:"rulesets,omitempty" yaml:"rulesets,omitempty"`
 		Templates []model.Template `json:"templates,omitempty" yaml:"templates,omitempty"`
 	}{Templates: template_list, Providers: provider_list, Rulesets: ruleset_list})
@@ -101,4 +102,52 @@ func Migrate(ent_client *ent.Client, bunt_client *buntdb.DB, logger *zap.Logger)
 		return nil, fmt.Errorf(`反序列化配置信息失败: [%s]`, err.Error())
 	}
 	return content, nil
+}
+func Import(content []byte, ent_client *ent.Client, logger *zap.Logger) ([]gin.H, error) {
+	setting := struct {
+		Providers []struct {
+			model.Provider `json:",inline" yaml:",inline"`
+			UUID           string           `json:"uuid,omitempty" yaml:"uuid,omitempty"`
+			Nodes          []map[string]any `json:"nodes,omitempty" yaml:"nodes,omitempty"`
+			Templates      []string         `json:"templates,omitempty" yaml:"templates,omitempty"`
+		} `json:"providers,omitempty" yaml:"providers,omitempty"`
+		Rulesets []struct {
+			model.Ruleset `json:",inline" yaml:",inline"`
+			Templates     []string `json:"templates,omitempty" yaml:"templates,omitempty"`
+		} `json:"rulesets,omitempty" yaml:"rulesets,omitempty"`
+		Templates []model.Template `json:"templates,omitempty" yaml:"templates,omitempty"`
+	}{}
+	if err := yaml.Unmarshal(content, &setting); err != nil {
+		logger.Error(fmt.Sprintf(`序列化配置文件失败: [%s]`, err.Error()))
+		return nil, fmt.Errorf(`序列化配置文件失败: [%s]`, err.Error())
+	}
+	res := []gin.H{}
+	for _, provider := range setting.Providers {
+		if err := ent_client.Provider.Create().SetName(provider.Name).SetPath(provider.Path).SetRemote(provider.Remote).SetUUID(provider.UUID).SetNodes(provider.Nodes).SetTemplates(provider.Templates).Exec(context.Background()); err != nil {
+			logger.Error(fmt.Sprintf(`添加机场"%s"失败: [%s]`, provider.Name, err.Error()))
+			res = append(res, gin.H{"status": false, "message": fmt.Sprintf(`添加机场"%s"失败: [%s]`, provider.Name, err.Error())})
+			continue
+		}
+		res = append(res, gin.H{"status": true, "message": fmt.Sprintf(`添加机场"%s"成功`, provider.Name)})
+	}
+	for _, ruleset := range setting.Rulesets {
+		if err := ent_client.Ruleset.Create().SetName(ruleset.Name).SetPath(ruleset.Path).SetRemote(ruleset.Remote).SetBinary(ruleset.Binary).SetUpdateInterval(ruleset.UpdateInterval).SetDownloadDetour(ruleset.DownloadDetour).SetTemplates(ruleset.Templates).Exec(context.Background()); err != nil {
+			logger.Error(fmt.Sprintf(`添加规则集%s失败: [%s]`, ruleset.Name, err.Error()))
+			res = append(res, gin.H{"status": false, "message": fmt.Sprintf(`添加规则集"%s"失败: [%s]`, ruleset.Name, err.Error())})
+			continue
+		}
+		res = append(res, gin.H{"status": true, "message": fmt.Sprintf(`添加规则集"%s"成功`, ruleset.Name)})
+	}
+	for _, template := range setting.Templates {
+		template_instance := ent_client.Template.Create()
+		template.CreateFillFields(template_instance)
+		if err := template_instance.SetInbounds(template.Inbounds).SetName(template.Name).SetOutboundGroups(template.OutboundsGroup).SetProviders(template.Providers).SetUpdated(true).Exec(context.Background()); err != nil {
+			logger.Error(fmt.Sprintf(`添加模板"%s"失败: [%s]`, template.Name, err.Error()))
+			res = append(res, gin.H{"status": false, "message": fmt.Sprintf(`添加模板"%s"失败: [%s]`, template.Name, err.Error())})
+			continue
+		}
+		res = append(res, gin.H{"status": true, "message": fmt.Sprintf(`添加模板"%s"成功`, template.Name)})
+
+	}
+	return res, nil
 }
--- route/migrate.go
+++ route/migrate.go
@@ -2,6 +2,7 @@ package route
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 	"sifu-box/control"
 	"sifu-box/ent"
@@ -29,8 +30,8 @@ func SettingMigrate(api *gin.RouterGroup, ent_client *ent.Client, bunt_client *b
 	}
 	migrate := api.Group("/migrate")
 	migrate.Use(middleware.JwtAuth(user.Key, logger))
-	migrate.GET("/fetch", func(c *gin.Context) {
-		content, err := control.Migrate(ent_client, bunt_client, logger)
+	migrate.GET("/export", middleware.AdminAuth(), func(c *gin.Context) {
+		content, err := control.Export(ent_client, bunt_client, logger)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
 			return
@@ -38,5 +39,29 @@ func SettingMigrate(api *gin.RouterGroup, ent_client *ent.Client, bunt_client *b
 		c.Header("Content-Type", "text/plain")
 		c.Header("Content-Disposition", `attachment; filename="migrate.yaml"`)
 		c.Data(http.StatusOK, "application/octet-stream", content)
+	})
+	migrate.POST("/import", middleware.AdminAuth(), func(c *gin.Context) {
+		file, err := c.FormFile("file")
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"message": fmt.Sprintf("获取文件表单失败: [%s]", err.Error())})
+			return
+		}
+		src, err := file.Open()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"message": fmt.Sprintf("打开文件失败: [%s]", err.Error())})
+			return
+		}
+		defer src.Close()
+		content, err := io.ReadAll(src)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"message": fmt.Sprintf("读取文件失败: [%s]", err.Error())})
+			return
+		}
+		res, err := control.Import(content, ent_client, logger)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+			return
+		}
+		c.JSON(http.StatusMultiStatus, res)
 	})
 }
